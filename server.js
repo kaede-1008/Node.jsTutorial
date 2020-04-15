9c4fa9f8dd6f149cdcabb627dcc277f66b8a0eea
kaede_1008 <s16t203@stu.kagawa-u.ac.jp>
2020-04-16 06:09:36 +0900

    png表示

--- server.js
+++ server.js
@@ -4,16 +4,26 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
+        var postData = ""
         var pathname = url.parse(request.url).pathname; //ルーティング取得
         console.log("Request " + pathname + " received");
 
-        // route(handle, pathname);
-
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        var content = route(handle, pathname);
-        response.write(content);
-        response.end();
+        request.setEncoding("utf8");
+        
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '"+
+            postDataChunk + "'.");
+        });
+        request.addListener("end", function() {
+            route(handle, pathname, response, postData);
+        });
     }
+    //     response.writeHead(200, {"Content-Type": "text/plain"});
+    //     var content = route(handle, pathname);
+    //     response.write(content);
+    //     response.end();
+    // }
     http.createServer(onRequest).listen(8888);
     console.log("server has started");
 }
